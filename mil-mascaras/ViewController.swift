af4c6c5175eea996a68f6624bf5aed1215d80f39
gallaugher <john.gallaugher@bc.edu>
2021-01-21 16:17:36 -0500

    Initial Commit

---
+++ mil-mascaras/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mil-mascaras
+//
+//  Created by John Gallaugher on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
